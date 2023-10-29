863c826018c19b888d8c6eaea5ff165b28fd5955
TDarw <142160091+TDarw@users.noreply.github.com>
2023-10-29 17:35:06 -0500

    Update menuScene.js

--- conversationsimulator/game/js/menuScene.js
+++ conversationsimulator/game/js/menuScene.js
@@ -7,8 +7,8 @@ export default class MenuScene extends Phaser.Scene {
 
   preload() {
     this.load.image("startbackground", "https://tdarw.github.io/conversationsimulatorassets/startbackground.png",);
-    this.load.image("arrow1", "https://tdarw.github.io/conversationsimulatorassets/arrow1.png",);
-    this.load.image("arrow2", "https://tdarw.github.io/conversationsimulatorassets/arrow2.png",);
+    this.load.image("arrow1", "https://tdarw.github.io/conversationsimulator/assets/arrow1.png",);
+    this.load.image("arrow2", "https://tdarw.github.io/conversationsimulator/assets/arrow2.png",);
   }
 
   create() {
