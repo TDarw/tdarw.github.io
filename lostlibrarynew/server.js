3da57def64977e671910e3f7ed1ec9a7590aac44
TDarw <142160091+TDarw@users.noreply.github.com>
2024-07-31 12:14:07 -0500

    Update server.js

--- lostlibrarynew/server.js
+++ lostlibrarynew/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config(); // Load environment variables from .env file
-console.log(process.env.MONGO_URI); // Log the MONGO_URI to ensure it's loaded
 
 const express = require('express');
 const mongoose = require('mongoose');
